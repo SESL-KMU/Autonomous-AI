eea9b3eaf455ff8073c62cacba2ec2328e657387
Yarmknky <tamsruna1@kookmin.ac.kr>
2021-01-03 17:46:21 +0900

    CV line detection example
    first commit

--- Day1/1_07_CV_LineDetection/CVlineDetection.py
+++ Day1/1_07_CV_LineDetection/CVlineDetection.py
@@ -10,77 +10,39 @@ import os, sys
 
 input_type = 'video' #'video' # 'image'
 
-# cap = cv2.VideoCapture('solidWhiteRight.mp4')
-# cap = cv2.VideoCapture('solidYellowLeft.mp4')
-# cap = cv2.VideoCapture('challenge.mp4')
-cap = cv2.VideoCapture('./test_images/0531_fps_20.mp4')
+cap = cv2.VideoCapture('./test_images/0015_fps_20.mp4')
 fit_result, l_fit_result, r_fit_result, L_lane, R_lane = [], [], [], [], []
-
 
 # Define the codec and create VideoWriter object
 # fourcc = cv2.VideoWriter_fourcc(*'mp4v') # Be sure to use the lower case
 # out = cv2.VideoWriter('output.mp4', fourcc, 20.0, ( 960, 540 ))
 
 def grayscale(img):
-    """Applies the Grayscale transform
-    This will return an image with only one color channel
-    but NOTE: to see the returned image as grayscale
-    you should call plt.imshow(gray, cmap='gray')"""
     return cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
 
-
 def canny(img, low_threshold, high_threshold):
-    """Applies the Canny transform"""
     return cv2.Canny(img, low_threshold, high_threshold)
 
-
 def gaussian_blur(img, kernel_size):
-    """Applies a Gaussian Noise kernel"""
     return cv2.GaussianBlur(img, (kernel_size, kernel_size), 0)
 
 
 def region_of_interest(img, vertices):
-    """
-    Applies an image mask.
-
-    Only keeps the region of the image defined by the polygon
-    formed from `vertices`. The rest of the image is set to black.
-    """
-    # defining a blank mask to start with
     mask = np.zeros_like(img)
 
-    # defining a 3 channel or 1 channel color to fill the mask with depending on the input image
     if len(img.shape) > 2:
         channel_count = img.shape[2]  # i.e. 3 or 4 depending on your image
         ignore_mask_color = (255,) * channel_count
     else:
         ignore_mask_color = 255
 
-    # filling pixels inside the polygon defined by "vertices" with the fill color
     cv2.fillPoly(mask, vertices, ignore_mask_color)
 
-    # returning the image only where mask pixels are nonzero
     masked_image = cv2.bitwise_and(img, mask)
     return masked_image
 
 
 def draw_lines(img, lines, color=[255, 0, 0], thickness=2):
-    """
-    NOTE: this is the function you might want to use as a starting point once you want to
-    average/extrapolate the line segments you detect to map out the full
-    extent of the lane (going from the result shown in raw-lines-example.mp4
-    to that shown in P1_example.mp4).
-
-    Think about things like separating line segments by their
-    slope ((y2-y1)/(x2-x1)) to decide which segments are part of the left
-    line vs. the right line.  Then, you can average the position of each of
-    the lines and extrapolate to the top and bottom of the lane.
-
-    This function draws `lines` with `color` and `thickness`.
-    Lines are drawn on the image inplace (mutates the image).
-    If you want to make the lines semi-transparent, think about combining
-    this function with the weighted_img() function below
-    """
     for line in lines:
         for x1, y1, x2, y2 in line:
             cv2.line(img, (x1, y1), (x2, y2), color, thickness)
@@ -92,10 +54,6 @@ def draw_circle(img, lines, color=[0, 0, 255]):
 
 
 def hough_lines(img, rho, theta, threshold, min_line_len, max_line_gap):
-    """
-    `img` should be the output of a Canny transform.
-    Returns an image with hough lines drawn.
-    """
     lines = cv2.HoughLinesP(img, rho, theta, threshold, np.array([]), minLineLength=min_line_len,
                             maxLineGap=max_line_gap)
     line_arr = np.zeros((img.shape[0], img.shape[1], 3), dtype=np.uint8)
@@ -103,19 +61,8 @@ def hough_lines(img, rho, theta, threshold, min_line_len, max_line_gap):
     return lines
 
 
-def weighted_img(img, initial_img, α=0.8, β=1., λ=0.):
-    """
-    `img` is the output of the hough_lines(), An image with lines drawn on it.
-    Should be a blank image (all black) with lines drawn on it.
-
-    `initial_img` should be the image before any processing.
-
-    The result image is computed as follows:
-
-    initial_img * α + img * β + λ
-    NOTE: initial_img and img must be the same shape!
-    """
-    return cv2.addWeighted(initial_img, α, img, β, λ)
+def weighted_img(img, initial_img, aa=0.8, bb=1., cc=0.):
+    return cv2.addWeighted(initial_img, aa, img, bb, cc)
 
 
 def Collect_points(lines):
@@ -270,8 +217,6 @@ def detect_lanes_img(img):
         dtype=np.int32)
     ROI_img = region_of_interest(img, vertices)
 
-    # Convert to grayimage
-    # g_img = grayscale(img)
 
     # Apply gaussian filter
     blur_img = gaussian_blur(ROI_img, 3)
@@ -291,7 +236,6 @@ def detect_lanes_img(img):
     # if can't find any lines
     if line_arr is None:
         return img
-    # draw_lines(img, line_arr, thickness=2)
 
     line_arr = np.squeeze(line_arr)
     # Get slope degree to separate 2 group (+ slope , - slope)
