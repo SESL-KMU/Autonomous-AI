2ca5ae8f7d4e048af0c0cc9a52c80b0954c7f925
Yarmknky <tamsruna1@kookmin.ac.kr>
2021-01-15 16:35:37 +0900

    Update 3_binarization.py

--- Day1/1_07_CV_LineDetection/line_example/3_binarization.py
+++ Day1/1_07_CV_LineDetection/line_example/3_binarization.py
@@ -8,8 +8,13 @@ _, otsu = cv2.threshold(image, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
 adap = cv2.adaptiveThreshold(image, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 7, 5)
 
 
+
+print('thres')
 cv2_imshow(thres) # 이미지 출력
+print('otsu')
 cv2_imshow(otsu) # 이미지 출력
+print('adap')
 cv2_imshow(adap) # 이미지 출력
+print('image')
 cv2_imshow(image) # 이미지 출력
 cv2.waitKey(0)
