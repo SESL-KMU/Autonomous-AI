6894bd0028e7a243f9047541f4e1311a72c4e012
qiaiqiai2 <48591634+qiaiqiai2@users.noreply.github.com>
2021-01-18 19:06:03 +0900

    cv_recognition

--- Day2/2_01_CV_Object_Recognition/extract_cars.py
+++ Day2/2_01_CV_Object_Recognition/extract_cars.py
@@ -24,18 +24,6 @@ threshold = 15
 min_line_len = 40
 max_line_gap = 20
 
-def ShowImage(name_of_image, image_, rate):
-    img_min = cv2.resize(image_, None, fx=rate, fy=rate, interpolation=cv2.INTER_CUBIC)
-    cv2.namedWindow(name_of_image, cv2.WINDOW_NORMAL)
-    cv2.imshow(name_of_image, img_min)
-    if cv2.waitKey(0) == 27:  # wait for ESC to exit
-        print('Not saved!')
-        cv2.destroyAllWindows()
-    elif cv2.waitKey(0) == ord('s'):  # wait for 's' to save and exit
-        cv2.imwrite(name_of_image + '.jpg', image_)  # save
-        print('Saved successfully!')
-        cv2.destroyAllWindows()
-
 def get_hog_features(img, orient_, pix_per__cell, cell_per__block, vis=False, feature_vec=True):
     if vis:
         features, hog_image = hog(img, orientations=orient_,
@@ -405,7 +393,7 @@ else:
     with open('model', 'wb') as f:
         pickle.dump(svc, f)
 
-test_image = cv2.imread('images/test7.jpg')
+test_image = cv2.imread('images/test6.jpg')
 
 windows = choose_window_size(test_image, x_start_stop=None, y_start__stop=y_start_stop, overlap=(0.6, 0.7))
 box = search_windows(test_image, windows, svc, X_scaler, color__space=color_space, spatial__size=spatial_size, hist__bins=hist_bins,
@@ -416,6 +404,7 @@ box = search_windows(test_image, windows, svc, X_scaler, color__space=color_spac
 
 heat_map, labels, drawn_image = draw_labeled_windows(test_image, box, threshold_=1)
 
-cv2.imshow('image', test_image)
-#cv2.imshow()
-ShowImage('images/image1', drawn_image, 1)
+cv2_imshow(test_image)
+img_min = cv2.resize(drawn_image, None, fx=1, fy=1, interpolation=cv2.INTER_CUBIC)
+cv2_imshow(img_min)
+cv2.waitKey(0)
