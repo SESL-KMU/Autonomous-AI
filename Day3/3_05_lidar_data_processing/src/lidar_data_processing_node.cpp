336ba2808348dfe2b7f3ad6b42ecc2b738a97e95
Hyun-Jeong Oh <ohj0306@kookmin.ac.kr>
2021-07-13 19:18:56 +0900

    Update lidar_data_processing_node.cpp

--- Day3/3_05_lidar_data_processing/src/lidar_data_processing_node.cpp
+++ Day3/3_05_lidar_data_processing/src/lidar_data_processing_node.cpp
@@ -31,26 +31,26 @@ typedef pcl::PointCloud<pcl::PointXYZ> PointCloud;
 using namespace std;
 int j = 0;
 
-PointCloud::Ptr Message (PointCloud::Ptr);
-PointCloud::Ptr RoI_Filtering (PointCloud::Ptr);
-PointCloud::Ptr RANSAC (PointCloud::Ptr);
+PointCloud::Ptr Message(PointCloud::Ptr);
+PointCloud::Ptr RoI_Filtering(PointCloud::Ptr);
+PointCloud::Ptr RANSAC(PointCloud::Ptr);
 pcl::PointCloud<pcl::PointXYZI> Clustering(PointCloud::Ptr);
 visualization_msgs::Marker Box(PointCloud::Ptr);
 visualization_msgs::MarkerArray msg_marker;
 
 
-int main(int argc, char** argv){
+int main(int argc, char** argv) {
 
 	char PcdFileName[1000];
 
-	ros::init (argc, argv, "lidar_data_processing_node");
+	ros::init(argc, argv, "lidar_data_processing_node");
 
 	ros::NodeHandle nh;
-	ros::Publisher pub = nh.advertise<PointCloud> ("pointcloud", 1);  					//원본 포인트 클라우드 publisher 생성
-	ros::Publisher pub_filter = nh.advertise<PointCloud> ("pointcloud_filter",1);  			//ROI 적용 클라우드 publisher 생성
-	ros::Publisher pub_RANSAC = nh.advertise<PointCloud> ("pointcloud_RANSAC",1);  			//RANSAC 적용 클라우드 publisher 생성
-	ros::Publisher pub_ec = nh.advertise<sensor_msgs::PointCloud2> ("pointcloud_ec",1);  		//EC 적용 클라우드 publisher 생성
-	ros::Publisher pub_marker = nh.advertise<visualization_msgs::MarkerArray> ("msg_marker", 100);//Detection Box 적용 Marker Array publisher 생성
+	ros::Publisher pub = nh.advertise<PointCloud>("pointcloud", 1);  					//원본 포인트 클라우드 publisher 생성
+	ros::Publisher pub_filter = nh.advertise<PointCloud>("pointcloud_filter", 1);  			//ROI 적용 클라우드 publisher 생성
+	ros::Publisher pub_RANSAC = nh.advertise<PointCloud>("pointcloud_RANSAC", 1);  			//RANSAC 적용 클라우드 publisher 생성
+	ros::Publisher pub_ec = nh.advertise<sensor_msgs::PointCloud2>("pointcloud_ec", 1);  		//EC 적용 클라우드 publisher 생성
+	ros::Publisher pub_marker = nh.advertise<visualization_msgs::MarkerArray>("msg_marker", 100);//Detection Box 적용 Marker Array publisher 생성
 
 	ros::Rate loop_rate(10);
 
@@ -58,26 +58,26 @@ int main(int argc, char** argv){
 	while (nh.ok())
 	{
 		msg_marker.markers.clear();
-		PointCloud::Ptr msg (new PointCloud);			//rviz에 전달되는 메세지 초기화
-		PointCloud::Ptr msg_filter (new PointCloud);  		//rviz에 전달되는 filter 메세지 초기화
-		PointCloud::Ptr msg_RANSAC (new PointCloud);  	//rviz에 전달되는 RANSAC 메세지 초기화
-		PointCloud::Ptr msg_ec (new PointCloud);  		//rviz에 전달되는 EC 메세지 초기화
+		PointCloud::Ptr msg(new PointCloud);			//rviz에 전달되는 메세지 초기화
+		PointCloud::Ptr msg_filter(new PointCloud);  		//rviz에 전달되는 filter 메세지 초기화
+		PointCloud::Ptr msg_RANSAC(new PointCloud);  	//rviz에 전달되는 RANSAC 메세지 초기화
+		PointCloud::Ptr msg_ec(new PointCloud);  		//rviz에 전달되는 EC 메세지 초기화
 
-		PointCloud::Ptr cloud (new PointCloud);  		// 원본 클라우드 데이터
-		PointCloud::Ptr cloud_filter (new PointCloud);  		// ROI 적용 클라우드 데이터
-		PointCloud::Ptr cloud_RANSAC (new PointCloud);  	//지면 제거 클라우드 데이터
+		PointCloud::Ptr cloud(new PointCloud);  		// 원본 클라우드 데이터
+		PointCloud::Ptr cloud_filter(new PointCloud);  		// ROI 적용 클라우드 데이터
+		PointCloud::Ptr cloud_RANSAC(new PointCloud);  	//지면 제거 클라우드 데이터
 		pcl::PointCloud<pcl::PointXYZI> cloud_ec;  		//군집화 클라우드 데이터
 
-/********************************************main********************************************/
+														/********************************************main********************************************/
 
-		sprintf(PcdFileName,"C:/catkin_ws/src/lidar_data_processing/Data_pcd/pcd_%d.pcd",j++);
-		pcl::io::loadPCDFile<pcl::PointXYZ> (PcdFileName, *cloud);  //pcd파일이 변경될 때마다 새로운 cloud 생성
-		cout<<j<<endl;
+		sprintf(PcdFileName, "C:/catkin_ws/src/lidar_data_processing/Data_pcd/pcd_%d.pcd", j++);
+		pcl::io::loadPCDFile<pcl::PointXYZ>(PcdFileName, *cloud);  //pcd파일이 변경될 때마다 새로운 cloud 생성
+		cout << j << endl;
 
 		//cloud_filter = RoI_Filtering(cloud);
 		//cloud_RANSAC = RANSAC(cloud_filter);
 		//cloud_ec = Clustering(cloud_RANSAC);
-		
+
 		//msg = Message(cloud);				//원본 클라우드 데이터 msg
 		//msg_filter = Message(cloud_filter);		//필터링 결과 클라우드 데이터 msg
 		//msg_RANSAC = Message(cloud_RANSAC);		//RANSAC 알고리즘 결과 클라우드 데이터 msg
@@ -95,60 +95,60 @@ int main(int argc, char** argv){
 		//pub_marker.publish(msg_marker);			//각 msg publish
 
 
-/********************************************************************************************/
+		/********************************************************************************************/
 		msg_marker.markers.clear();
-		ros::spinOnce ();
-		loop_rate.sleep ();
-		
-		if (j>40) j=0;
+		ros::spinOnce();
+		loop_rate.sleep();
+
+		if (j>40) j = 0;
 
 	}
 }
 
-		
+
 
 /*********************************************************Message*********************************************************/
 
-PointCloud::Ptr Message (PointCloud::Ptr cloud){
+PointCloud::Ptr Message(PointCloud::Ptr cloud) {
 
 	double x_cloud; double y_cloud; double z_cloud;  	//포인트 클라우드 데이터 시각화를 위한 변수 선언
-	PointCloud::Ptr msg (new PointCloud);			//메세지 초기화
+	PointCloud::Ptr msg(new PointCloud);			//메세지 초기화
 
 	msg->header.frame_id = "map";
 	msg->height = cloud->height;
 	msg->width = cloud->width;
 
-	for (size_t i = 0; i < cloud->size (); i++) {
+	for (size_t i = 0; i < cloud->size(); i++) {
 		x_cloud = cloud->points[i].x;
 		y_cloud = cloud->points[i].y;
 		z_cloud = cloud->points[i].z;
-		msg->points.push_back (pcl::PointXYZ (x_cloud, y_cloud, z_cloud));
+		msg->points.push_back(pcl::PointXYZ(x_cloud, y_cloud, z_cloud));
 	}
-	
+
 	return msg;
 };
 
 /******************************************************RoI Filtering******************************************************/
 
-PointCloud::Ptr RoI_Filtering(PointCloud::Ptr cloud){
+PointCloud::Ptr RoI_Filtering(PointCloud::Ptr cloud) {
 
 	pcl::PassThrough<pcl::PointXYZ> pass;  		//ROI 적용을 위한 객체 생성
-	PointCloud::Ptr cloud_filter (new PointCloud);  	// ROI 적용 클라우드 데이터
+	PointCloud::Ptr cloud_filter(new PointCloud);  	// ROI 적용 클라우드 데이터
 
-	pass.setInputCloud (cloud);		//입력 
-	pass.setFilterFieldName ("z");		//적용할 좌표 축
-	pass.setFilterLimits (-3, 5);		//적용할 값 (최소, 최대 값)
-	pass.filter (*cloud_filter); 		//필터 적용 
+	pass.setInputCloud(cloud);		//입력 
+	pass.setFilterFieldName("z");		//적용할 좌표 축
+	pass.setFilterLimits(-3, 5);		//적용할 값 (최소, 최대 값)
+	pass.filter(*cloud_filter); 		//필터 적용 
 
-	pass.setInputCloud (cloud_filter);	//입력 
-	pass.setFilterFieldName ("y");		//적용할 좌표 축
-	pass.setFilterLimits (-5, 2.8);		//적용할 값 (최소, 최대 값)
-	pass.filter (*cloud_filter);		//필터 적용 
+	pass.setInputCloud(cloud_filter);	//입력 
+	pass.setFilterFieldName("y");		//적용할 좌표 축
+	pass.setFilterLimits(-5, 2.8);		//적용할 값 (최소, 최대 값)
+	pass.filter(*cloud_filter);		//필터 적용 
 
-	pass.setInputCloud (cloud_filter);	//입력 
-	pass.setFilterFieldName ("x");		//적용할 좌표 축
-	pass.setFilterLimits (-20, 30);		//적용할 값 (최소, 최대 값)
-	pass.filter (*cloud_filter);		//필터 적용 
+	pass.setInputCloud(cloud_filter);	//입력 
+	pass.setFilterFieldName("x");		//적용할 좌표 축
+	pass.setFilterLimits(-20, 30);		//적용할 값 (최소, 최대 값)
+	pass.filter(*cloud_filter);		//필터 적용 
 
 	return cloud_filter;
 };
@@ -156,30 +156,30 @@ PointCloud::Ptr RoI_Filtering(PointCloud::Ptr cloud){
 
 /******************************************************RANSAC Algorithm******************************************************/
 
-PointCloud::Ptr RANSAC(PointCloud::Ptr cloud){
+PointCloud::Ptr RANSAC(PointCloud::Ptr cloud) {
 
 	pcl::SACSegmentation<pcl::PointXYZ> seg;  							//RANSAC 적용을 위한 객체 생성
-	PointCloud::Ptr cloud_inliner (new PointCloud);  	//지면 정보 클라우드 데이터
-	PointCloud::Ptr cloud_inliner_neg (new PointCloud);  	//지면 제거 클라우드 데이터
-	pcl::ModelCoefficients::Ptr coefficients (new pcl::ModelCoefficients ());
-	pcl::PointIndices::Ptr inliers (new pcl::PointIndices ());					// Object for storing the plane model coefficients.
+	PointCloud::Ptr cloud_inliner(new PointCloud);  	//지면 정보 클라우드 데이터
+	PointCloud::Ptr cloud_inliner_neg(new PointCloud);  	//지면 제거 클라우드 데이터
+	pcl::ModelCoefficients::Ptr coefficients(new pcl::ModelCoefficients());
+	pcl::PointIndices::Ptr inliers(new pcl::PointIndices());					// Object for storing the plane model coefficients.
 
 
-	seg.setOptimizeCoefficients (true);		// Enable model coefficient refinement (optional).
-	seg.setInputCloud (cloud);			//입력 
-	seg.setModelType (pcl::SACMODEL_PLANE);		//적용 모델 (지면을 제거하기 위한 plane 모델 사용)
-	seg.setMethodType (pcl::SAC_RANSAC);		//Method Type RANSAC 사용
-	seg.setMaxIterations (300);			//최대 실행 수
-	seg.setDistanceThreshold (0.16);		//inlier로 처리할 거리 정보
-	//seg.setRadiusLimits(0, 0.1);     		// cylinder경우, Set minimum and maximum radii of the cylinder.
-	seg.segment (*inliers, *coefficients);		//세그멘테이션 적용 
+	seg.setOptimizeCoefficients(true);		// Enable model coefficient refinement (optional).
+	seg.setInputCloud(cloud);			//입력 
+	seg.setModelType(pcl::SACMODEL_PLANE);		//적용 모델 (지면을 제거하기 위한 plane 모델 사용)
+	seg.setMethodType(pcl::SAC_RANSAC);		//Method Type RANSAC 사용
+	seg.setMaxIterations(300);			//최대 실행 수
+	seg.setDistanceThreshold(0.16);		//inlier로 처리할 거리 정보
+										//seg.setRadiusLimits(0, 0.1);     		// cylinder경우, Set minimum and maximum radii of the cylinder.
+	seg.segment(*inliers, *coefficients);		//세그멘테이션 적용 
 	pcl::copyPointCloud<pcl::PointXYZ>(*cloud, *inliers, *cloud_inliner);  //지면정보 추출
 
 	pcl::ExtractIndices<pcl::PointXYZ> extract;
-	extract.setInputCloud (cloud);
-	extract.setIndices (inliers);
-	extract.setNegative (true);//false
-	extract.filter (*cloud_inliner_neg);
+	extract.setInputCloud(cloud);
+	extract.setIndices(inliers);
+	extract.setNegative(true);//false
+	extract.filter(*cloud_inliner_neg);
 
 
 	return cloud_inliner_neg;
@@ -188,33 +188,33 @@ PointCloud::Ptr RANSAC(PointCloud::Ptr cloud){
 
 /******************************************************Clustering******************************************************/
 
-pcl::PointCloud<pcl::PointXYZI> Clustering(PointCloud::Ptr cloud){
+pcl::PointCloud<pcl::PointXYZI> Clustering(PointCloud::Ptr cloud) {
 	// 탐색을 위한 KdTree 오브젝트 생성 //Creating the KdTree object for the search method of the extraction
-	pcl::search::KdTree<pcl::PointXYZ>::Ptr tree (new pcl::search::KdTree<pcl::PointXYZ>);
-	tree->setInputCloud (cloud);  //KdTree 생성 
+	pcl::search::KdTree<pcl::PointXYZ>::Ptr tree(new pcl::search::KdTree<pcl::PointXYZ>);
+	tree->setInputCloud(cloud);  //KdTree 생성 
 
 	std::vector<pcl::PointIndices> cluster_indices;       // 군집화된 결과물의 Index 저장, 다중 군집화 객체는 cluster_indices[0] 순으로 저장 
 
-	// 군집화 오브젝트 생성  
+														  // 군집화 오브젝트 생성  
 	pcl::EuclideanClusterExtraction<pcl::PointXYZ> ec;
-	ec.setInputCloud (cloud);       	// 입력   
-	ec.setClusterTolerance (0.7);
-	ec.setMinClusterSize (100);    		// 최소 포인트 수 
-	ec.setMaxClusterSize (6000);  		// 최대 포인트 수
-	ec.setSearchMethod (tree);     		// 위에서 정의한 탐색 방법 지정 
-	ec.extract (cluster_indices);  		// 군집화 적용 
+	ec.setInputCloud(cloud);       	// 입력   
+	ec.setClusterTolerance(0.7);
+	ec.setMinClusterSize(100);    		// 최소 포인트 수 
+	ec.setMaxClusterSize(6000);  		// 최대 포인트 수
+	ec.setSearchMethod(tree);     		// 위에서 정의한 탐색 방법 지정 
+	ec.extract(cluster_indices);  		// 군집화 적용 
 
 
 	pcl::PointCloud<pcl::PointXYZI> TotalCloud;
 	msg_marker.markers.clear();
 	int k = 0;
 
-	for (std::vector<pcl::PointIndices>::const_iterator it = cluster_indices.begin(); it != cluster_indices.end(); ++it){
-		
+	for (std::vector<pcl::PointIndices>::const_iterator it = cluster_indices.begin(); it != cluster_indices.end(); ++it) {
+
 		//PointCloud::Ptr box_points (new PointCloud);
 		//visualization_msgs::Marker box_marker;
-		
-		for (std::vector<int>::const_iterator pit = it->indices.begin(); pit != it->indices.end(); ++pit){
+
+		for (std::vector<int>::const_iterator pit = it->indices.begin(); pit != it->indices.end(); ++pit) {
 
 			pcl::PointXYZ pt = cloud->points[*pit];
 			pcl::PointXYZI pt2;
@@ -223,8 +223,8 @@ pcl::PointCloud<pcl::PointXYZI> Clustering(PointCloud::Ptr cloud){
 
 			//box_points->points.push_back(pcl::PointXYZ(pt2.x, pt2.y, pt2.z));
 			TotalCloud.push_back(pt2);
-		      }
-		
+		}
+
 		//box_marker = Box(box_points);
 		//box_marker.id = k;
 		//msg_marker.markers.push_back(box_marker);
@@ -239,7 +239,7 @@ pcl::PointCloud<pcl::PointXYZI> Clustering(PointCloud::Ptr cloud){
 
 /******************************************************Detection Box******************************************************/
 
-visualization_msgs::Marker Box(PointCloud::Ptr box_points){
+visualization_msgs::Marker Box(PointCloud::Ptr box_points) {
 	pcl::MomentOfInertiaEstimation<pcl::PointXYZ> feature_extractor;
 	//feature_extractor.setInputCloud(convert_xyz);
 	feature_extractor.setInputCloud(box_points);
@@ -256,15 +256,15 @@ visualization_msgs::Marker Box(PointCloud::Ptr box_points){
 	Eigen::Vector3f major_vector, middle_vector, minor_vector;
 	Eigen::Vector3f mass_center;
 
-	feature_extractor.getMomentOfInertia(moment_of_inertia); 
+	feature_extractor.getMomentOfInertia(moment_of_inertia);
 	feature_extractor.getEccentricity(eccentricity);
 	feature_extractor.getOBB(min_point_OBB, max_point_OBB, position_OBB, rotational_matrix_OBB);
 	feature_extractor.getEigenValues(major_value, middle_value, minor_value);
 	feature_extractor.getEigenVectors(major_vector, middle_vector, minor_vector);
 	feature_extractor.getMassCenter(mass_center);
 
-	Eigen::Vector3f position (position_OBB.x, position_OBB.y, position_OBB.z);
-	Eigen::Quaternionf quat (rotational_matrix_OBB);
+	Eigen::Vector3f position(position_OBB.x, position_OBB.y, position_OBB.z);
+	Eigen::Quaternionf quat(rotational_matrix_OBB);
 
 	//visualization marker
 	std::string ns;
@@ -273,19 +273,19 @@ visualization_msgs::Marker Box(PointCloud::Ptr box_points){
 	bbx_marker.header.frame_id = "map";
 	bbx_marker.header.stamp = ros::Time::now();
 	bbx_marker.ns = ns;
-	
+
 	bbx_marker.type = visualization_msgs::Marker::CUBE;
 	bbx_marker.action = visualization_msgs::Marker::ADD;
 
-	bbx_marker.pose.position.x =  position_OBB.x;
-	bbx_marker.pose.position.y =  position_OBB.y;
-	bbx_marker.pose.position.z =  position_OBB.z;
+	bbx_marker.pose.position.x = position_OBB.x;
+	bbx_marker.pose.position.y = position_OBB.y;
+	bbx_marker.pose.position.z = position_OBB.z;
 	bbx_marker.pose.orientation.x = quat.x();
 	bbx_marker.pose.orientation.y = quat.y();
 	bbx_marker.pose.orientation.z = quat.z();
 	bbx_marker.pose.orientation.w = quat.w();
 
-	if((max_point_OBB.x - min_point_OBB.x < 5) && (max_point_OBB.x - min_point_OBB.x > 1))
+	if ((max_point_OBB.x - min_point_OBB.x < 5) && (max_point_OBB.x - min_point_OBB.x > 1))
 		bbx_marker.scale.x = (max_point_OBB.x - min_point_OBB.x);
 	bbx_marker.scale.y = (max_point_OBB.y - min_point_OBB.y);
 	bbx_marker.scale.z = (max_point_OBB.z - min_point_OBB.z);
