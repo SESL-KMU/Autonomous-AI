65b5420ccdd31924366c55deeaa55813ccb276d2
apex20 <apex@kookmin.ac.kr>
2021-08-09 01:04:50 +0900

    Add Q-learning on stochastic scenario

---
+++ Day4/4_06_RL/RL/Q-learning3.py
@@ -0,0 +1,55 @@
+import gym
+import numpy as np
+import matplotlib.pyplot as plt
+from gym.envs.registration import register
+
+register(
+    id='FrozenLake-v3',
+    entry_point='gym.envs.toy_text:FrozenLakeEnv',
+    kwargs={'map_name': '4x4', 'is_slippery': False}
+)
+
+env = gym.make('FrozenLake-v3')
+
+# Inistialize Q-table with all zeros, shape = [States num, 4(left,down,right,up)]
+Q = np.zeros([env.observation_space.n, env.action_space.n])
+# Set learning parameters
+lr = 0.3
+dis = 0.99
+num_episodes = 2000
+
+# create lists to contain total rewards and steps per episode
+rList = []
+for i in range(num_episodes):
+    # Reset environment and get first new observation
+    state = env.reset()
+    rAll = 0
+    done = False
+
+    e = 1.0 / ((i // 100) + 1)
+
+    # The Q-Table learning algorithm
+    while not done:
+        if np.random.rand(1) < e:
+            action = env.action_space.sample()
+        else:
+            action = np.argmax(Q[state, :] + np.random.randn(1, env.action_space.n) / (i + 1))
+
+        # Get new state and reward from environment
+        new_state, reward, done, _ = env.step(action)
+
+        # Update Q-Table with new knowledge using learning rate
+        Q[state, action] = (1 - lr) * Q[state, action] + lr * (reward + dis * np.max(Q[new_state, :]))
+
+        rAll += reward
+        state = new_state
+
+    rList.append(rAll)
+
+print("Success rate: " + str(sum(rList) / num_episodes))
+print("Final Q-Table Values")
+print("LEFT DOWN RIGHT UP")
+print(Q)
+plt.title("Success rate: " + str(sum(rList) / num_episodes))
+plt.bar(range(len(rList)), rList, color="blue")
+plt.show()
