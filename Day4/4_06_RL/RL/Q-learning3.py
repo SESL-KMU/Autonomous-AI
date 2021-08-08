91cc294dfda86cfce98d94b2f27ace32f406ba4f
apex20 <apex@kookmin.ac.kr>
2021-08-09 01:22:38 +0900

    Update Q-learning

--- Day4/4_06_RL/RL/Q-learning3.py
+++ Day4/4_06_RL/RL/Q-learning3.py
@@ -6,7 +6,7 @@ from gym.envs.registration import register
 register(
     id='FrozenLake-v3',
     entry_point='gym.envs.toy_text:FrozenLakeEnv',
-    kwargs={'map_name': '4x4', 'is_slippery': False}
+    kwargs={'map_name': '4x4', 'is_slippery': True}
 )
 
 env = gym.make('FrozenLake-v3')
@@ -14,7 +14,7 @@ env = gym.make('FrozenLake-v3')
 # Inistialize Q-table with all zeros, shape = [States num, 4(left,down,right,up)]
 Q = np.zeros([env.observation_space.n, env.action_space.n])
 # Set learning parameters
-lr = 0.3
+lr = 0.8
 dis = 0.99
 num_episodes = 2000
 
@@ -26,14 +26,10 @@ for i in range(num_episodes):
     rAll = 0
     done = False
 
-    e = 1.0 / ((i // 100) + 1)
-
     # The Q-Table learning algorithm
     while not done:
-        if np.random.rand(1) < e:
-            action = env.action_space.sample()
-        else:
-            action = np.argmax(Q[state, :] + np.random.randn(1, env.action_space.n) / (i + 1))
+        # Choose an action by dreedily (with noise) picking from Q table
+        action = np.argmax(Q[state, :] + np.random.randn(1, env.action_space.n) / (i + 1))
 
         # Get new state and reward from environment
         new_state, reward, done, _ = env.step(action)
